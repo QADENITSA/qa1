30ddde66cec3a5e9a2b5c4d6958d936f07e22afe
plamen22 <plamenshishmanov@gmail.com>
2022-12-08 19:42:50 +0200

    Add UnionString

---
+++ src/main/java/Home/work/UnionString.java
@@ -0,0 +1,10 @@
+package Home.work;
+
+public class UnionString {
+    public static void main(String args[]) {
+        String firstName = "Dennitsa";
+        String secondName = "Veselinova";
+        String lastName = "Gencheva";
+        System.out.println(firstName + " " + secondName + " " + lastName);
+    }
+}
